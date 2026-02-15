a8af97a1bbc388d1d3aa7dac5b009e8027268b3f
Yuvaraj M N <yuvarajmn737@gmail.com>
2026-02-15 22:34:52 +0530

    Fixed login error

--- app.js
+++ app.js
@@ -62,11 +62,14 @@ const sessionOptions={
     secret:process.env.SECRET,
     resave:false,
     saveUninitialized:false,
-    cookie:{
-        expires:Date.now()+7*24*60*60*1000,
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-    }
+    cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax"
+}
+
 };
 
 app.use(cookieParser());
